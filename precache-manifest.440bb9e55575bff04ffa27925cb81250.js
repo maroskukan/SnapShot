ed088955f51a27cc5fcb415d4265458af7671220
maroskukan <maroskukan@users.noreply.github.com>
2021-01-08 13:42:28 +0000

    deploy: 587e70c4d01c5afe3ed52fbdbe09599550704745

---
+++ precache-manifest.440bb9e55575bff04ffa27925cb81250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f67c7d9edf1b3bd9d16af9abed7d4c8",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "3d53ba9aab738a2d0e7c",
+    "url": "/SnapShot/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "d181d5d7af02efcd3dab",
+    "url": "/SnapShot/static/js/2.c619e43e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnapShot/static/js/2.c619e43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d53ba9aab738a2d0e7c",
+    "url": "/SnapShot/static/js/main.fcfa99e5.chunk.js"
+  },
+  {
+    "revision": "17f44848bce1e5215d0e",
+    "url": "/SnapShot/static/js/runtime-main.136163d9.js"
+  }
+]);
